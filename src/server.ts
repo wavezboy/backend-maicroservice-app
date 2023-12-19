0ea0bc5c7a151fd5964a83daa75daba1dcd9e3f6
wavezboy <sanusifatah16@gmail.com>
2023-12-20 00:04:29 +0100

    server define without variable

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
-import express from "express";
+import mongoose from "mongoose";
+import app from "./app";
 
-const app = express();
-
-export default app;
+mongoose
+  .connect("")
+  .then(() => {
+    console.log("mongoose connected");
+    app.listen(8000, () => {
+      console.log("connected to 8000");
+    });
+  })
+  .catch(console.error);
